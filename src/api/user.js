83a75099a081ab4726386a16e4a09679f1429b62
Rafael Acosta <rafael.r.acosta.m.93@gmail.com>
2020-05-24 11:43:27 -0400

    se consume endpoint para obtener todos los usuarios

--- src/api/user.js
+++ src/api/user.js
@@ -49,4 +49,28 @@ export const signInApi = (data) => {
         })
 }
 
+export const getUsersApi = (token) => {
+    const url = `${BASE_PATH}/${API_VERSION}/users`;
+    const params = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: token
+        }
+    };
+
+    return fetch(url, params).then(response => {
+        return response.json();
+    })
+        .then(result => {
+            if (result.message) {
+                return result.message;
+            }
+            return result;
+        })
+        .catch(err => {
+            return err.message;
+        })
+}
+
 export default signUpApi
--- src/pages/Admin/Users/Users.js
+++ src/pages/Admin/Users/Users.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {getAccessTokenApi} from "../../../api/auth";
+import {getUsersApi} from "../../../api/user";
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
+    const token = getAccessTokenApi();
+    console.log(users)
+
+    useEffect(  () => {
+        const fetchData = async () => {
+            const res = await getUsersApi(token);
+            setUsers(res);
+        }
+        fetchData();
+    }, [token])
 
     return (
         <div className="users">
