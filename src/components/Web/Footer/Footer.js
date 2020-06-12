64269d06d29a4db7b473a593438158729ce842be
Rafael Acosta <rafael.r.acosta.m.93@gmail.com>
2020-06-12 11:15:36 -0400

    Deploy v0.0.1

--- src/components/Web/Footer/Footer.js
+++ src/components/Web/Footer/Footer.js
@@ -25,7 +25,7 @@ const Footer = () => {
                     </Row>
                     <Row className="footer__copyright">
                         <Col md={12}>
-                            © 2020 ALL RIGHTS RESERVED
+                            © 2020 ALL RIGHTS RESERVED.
                         </Col>
                         <Col md={12}>
                             RAFAEL ACOSTA MARTINEZ | DESARROLLADOR WEB
